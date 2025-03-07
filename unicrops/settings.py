1daa8a3d5c82b965e2de1de6444b7603cb92faf4
abbeyzino51@gmal.com <abbeyzino51@gmal.com>
2025-03-07 01:43:15 +0100

    Updated social auth settings

--- unicrops/settings.py
+++ unicrops/settings.py
@@ -38,7 +38,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS =['*', '127.0.0.1', 'localhost', '10.0']
+ALLOWED_HOSTS =['*', '127.0.0.1', 'localhost', '10.0', "unicrops.onrender.com"]
 
 
 # Application definition
